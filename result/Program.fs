7b4e13c19e0e0724d4c0d93feac85c16f264a94f
Leonardo Valencia <leov@gammawizard.com>
2025-10-15 17:59:24 -0500

    Version inicial de result

---
+++ result/Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+let leerBaseDeDatosNacionesUnidas() =
+    let everythingIsOk = true    
+    if everythingIsOk then 
+        Ok 42
+    else    
+        Error "Problema con internet"
+let r = leerBaseDeDatosNacionesUnidas()
+
+match r with 
+| Ok resultado -> printfn $"Perfector el resultado es {resultado}"
+| Error mensaje -> printfn $"{mensaje}"
