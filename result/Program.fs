0f4bf8c0357396c881bfc29ab1c38c82fa1d5910
Leonardo Valencia <leov@gammawizard.com>
2025-10-15 18:33:04 -0500

    Agregar comentario

--- result/Program.fs
+++ result/Program.fs
@@ -1,4 +1,5 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
+﻿
+open System
 
 let leerBaseDeDatosNacionesUnidas() =
     let everythingIsOk = true    
@@ -11,3 +12,7 @@ let r = leerBaseDeDatosNacionesUnidas()
 match r with 
 | Ok resultado -> printfn $"Perfector el resultado es {resultado}"
 | Error mensaje -> printfn $"{mensaje}"
+
+//
+// Vamos a crear un simulador de errores
+//
