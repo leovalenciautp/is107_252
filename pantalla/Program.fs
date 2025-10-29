63a925c406528c2bb56a0c0e45807f7064f93666
Leonardo Valencia <leov@gammawizard.com>
2025-10-28 22:23:21 -0400

    Version inicial pantalla

---
+++ pantalla/Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+
+let centerX = Console.BufferWidth/2
+let centerY = Console.BufferHeight/2
+
+let originalForeground = Console.ForegroundColor
+let originalBackground = Console.BackgroundColor
+
+Console.BackgroundColor <- ConsoleColor.Blue
+Console.Clear()
+Console.SetCursorPosition(centerX-4,centerY)
+Console.ForegroundColor <- ConsoleColor.Red
+Console.BackgroundColor <- ConsoleColor.Yellow
+Console.Write "Hola "
+Console.ForegroundColor <- ConsoleColor.Green
+Console.BackgroundColor <- ConsoleColor.Black
+Console.WriteLine "Mundo"
+Console.ReadLine() |> ignore
+
+//
+// Cuando el programa termine, es bueno restaurar
+//
+Console.ForegroundColor <- originalForeground
+Console.BackgroundColor <- originalBackground
+Console.Clear()
+
